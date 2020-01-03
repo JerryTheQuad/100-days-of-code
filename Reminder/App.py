6ef3a66a5f674073224017ac438443f3dbbc33f0
JerryTheQuad <42748875+JerryTheQuad@users.noreply.github.com>
2020-01-03 12:00:35 +0300

    Update App.py

--- Reminder/App.py
+++ Reminder/App.py
@@ -4,7 +4,7 @@ print('What shall I remind you about?')
 text = str(input())
 
 print('In how many minutes?')
-#Multiplying the next variable by 60 because time.sleep needs to count in seconds
+#Multiplying the next variable by 60 because time.sleep counts in seconds
 local_time = float(input()) * 60
 
 time.sleep(local_time)
