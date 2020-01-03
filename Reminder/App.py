41995093cf81835073622a0690828631fd436743
JerryTheQuad <42748875+JerryTheQuad@users.noreply.github.com>
2020-01-03 12:00:06 +0300

    Create App.py

---
+++ Reminder/App.py
@@ -0,0 +1,12 @@
+import time
+
+print('What shall I remind you about?')
+text = str(input())
+
+print('In how many minutes?')
+#Multiplying the next variable by 60 because time.sleep needs to count in seconds
+local_time = float(input()) * 60
+
+time.sleep(local_time)
+
+print(text)
